4aeb6d05b826298ba705e16ba79e69d720b90825
JOSHWAJAYASEELAN S <joshwajayaseelandjs@gmail.com>
2024-05-13 18:46:31 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nucleotide-Sequence-generator
+
+Website  Link : https://nucleotide-generator.herokuapp.com/
+
+This project is to generate random sequence of DNA nucleotides (i.e. A, G, T, C) and also to find reverse complement of that particular sequence.
+
+This project is made out of flask and **if you love to contribute please make a new issue and let me know.**
+
+## Website Preview
+
+<p align="center">
+        <img src="https://i.imgur.com/M8rcczr.png" title="Nucleotide seq generator">
+</p>
+
+### This Project was made with help of these contributors
+
+<p align="center">
+        <a href="https://github.com/logan1x/Nucleotide-Sequence-generator/graphs/contributors">
+                <img src="https://contributors-img.web.app/image?repo=logan1x/Nucleotide-Sequence-generator" />
+        </a>
+</p>
---
+++ hello.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, render_template,request
+import random
+
+app = Flask(__name__)
+
+
+nucleotide = ["A","G","T","C"]
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def hello1():
+    if int(request.form['len']) < 1:
+        return render_template("index.html", error="Number has to be greater than 0")
+
+    if int(request.form['len']) > 1000000:
+        return render_template("index.html", error="Number has to be less than 1,000,000")
+     
+    len = request.form['len']
+    seq = ''.join([random.choice(nucleotide)
+               for nuc in range(int(len))])
+    rev_seq = seq[::-1]
+    rev_seq = rev_seq.replace("A","t").replace("T","a").replace("G","c").replace("C","g").upper()
+    # processed_text = text.upper()
+    x = 1
+    return render_template("index.html", seq = seq , rev_seq = rev_seq , x=x)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug="True")
+
